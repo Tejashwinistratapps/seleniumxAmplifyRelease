bf8b768b477491d9f99c4619bb4d969f1ab4f28e
lakshmig <lakshmig@LAP032.stratapps.com>
2019-06-06 15:20:04 +0530

    login for commit-release

--- src/test/java/com/xamplify/login/Login.java
+++ src/test/java/com/xamplify/login/Login.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebDriver;
 public class Login {
 	 WebDriver driver = Instance.getInstance();
 
-		Properties prop = PropertiesFile.readPropertyFile("datafile.properties");
+		Properties prop = PropertiesFile.readPropertyFile("rdata.properties");
 	@Test(priority=1)
 	public void start() throws InterruptedException {
 		
@@ -26,8 +26,8 @@ public class Login {
 	@Test(priority=2,enabled=true)
 	public void login() throws InterruptedException, SQLException {
 
-		driver.findElement(By.xpath(prop.getProperty("loginclick"))).click();
-		Thread.sleep(5000);
+		//driver.findElement(By.xpath(prop.getProperty("loginclick"))).click();
+		//Thread.sleep(5000);
 		driver.findElement(By.id(prop.getProperty("email"))).sendKeys(prop.getProperty("user.name"));
 		driver.findElement(By.id(prop.getProperty("password"))).sendKeys(prop.getProperty("user.password"));
 		driver.findElement(By.id(prop.getProperty("signin"))).click();
