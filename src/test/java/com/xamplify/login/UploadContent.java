715f818de009366571861050a1dcd45803dcaeb3
lakshmig <lakshmig@LAP032.stratapps.com>
2020-02-03 14:28:07 +0530

    Commit for Upload Content

--- src/test/java/com/xamplify/login/UploadContent.java
+++ src/test/java/com/xamplify/login/UploadContent.java
@@ -1,6 +1,7 @@
 package com.xamplify.login;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -11,78 +12,63 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class UploadContent 
 {
 
-	static WebDriver driver = Instance.getInstance();
-	static Properties properties = PropertiesFile.readPropertyFile("datafile.properties");
-	public  static Logger logger = LoggerFactory.getLogger(UploadVideo.class);
 
-	public static void main(String[] args) throws InterruptedException, IOException {
+	 WebDriver driver = Instance.getInstance();
+	 
+		Properties properties = PropertiesFile.readPropertyFile("rdata.properties");
+		
 
-		logger.debug("opening the browser");
-
-		driver.manage().window().maximize();
-		logger.debug("Entering the URL");
-
-		driver.get("https://xamplify.io/");
-		driver.findElement(By.xpath("//a[@class='loginTF']")).click();
-		Thread.sleep(10000);
-		logger.debug("Login");
-
-		WebElement usernameElement = driver.findElement(By.xpath("//input[@id='username']"));
-		WebElement passwordElement = driver.findElement(By.xpath("//input[@id='password']"));
-		usernameElement.sendKeys("dtejashwini@stratapps.com");
-		passwordElement.sendKeys("Xamplify@11");
-		Thread.sleep(3000);
-
-		driver.findElement(By.xpath("/html[1]/body[1]/app-root[1]/app-login[1]/div[1]/div[1]/div[2]/form[1]/button[1]"))
-		.click();
+		@BeforeMethod
+		
+		public void upload_Content() throws InterruptedException, SQLException, IOException
+		{
+			
 		Thread.sleep(5000);
-		logger.debug("login successful");
-		Thread.sleep(5000);
-
-		//UploadClick//
-		Thread.sleep(3000);
-		WebElement ele = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));
+		WebElement ele = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));//hover to content
 		Actions act = new Actions(driver);
 		act.moveToElement(ele).perform();
 		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("Upload_Click"))).click();
+		driver.findElement(By.xpath(properties.getProperty("Upload_Click"))).click();//click for upload content
 		Thread.sleep(10000);
 
 		//Browse//
 		
 
-		driver.findElement(By.xpath(properties.getProperty("cBrowse_click"))).click();
+		driver.findElement(By.xpath(properties.getProperty("cBrowse_click"))).click();//browse to click
 
 		Thread.sleep(3000);
 
-		Runtime.getRuntime().exec(properties.getProperty("cAutoIt")); 
+		Runtime.getRuntime().exec(properties.getProperty("cAutoIt")); //to run auto it file
 		System.out.println("successfully uploaded the Content from local");
 		Thread.sleep(8000);
-
+		}
+		
+		@Test(priority=56,enabled=true)
+		public void upload_Dropbox() throws InterruptedException {
+			
+		
 		//UploadClick-1//
 		Thread.sleep(3000);
-		WebElement ele1 = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));
+		WebElement ele1 = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));//hover to content
 		Actions act1 = new Actions(driver);
 		act1.moveToElement(ele1).perform();
 		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("Upload_Click"))).click();
-		Thread.sleep(10000);
+		driver.findElement(By.xpath(properties.getProperty("Upload_Click"))).click();//click for upload content
+		Thread.sleep(5000);
 
 		//DropBox//
 
-		driver.findElement(By.xpath(properties.getProperty("cdropbox_click"))).click();
+		driver.findElement(By.xpath(properties.getProperty("cdropbox_click"))).click();//click for drop box
 
 		Thread.sleep(5000);
 
-		Set<String> hashset3 = (HashSet<String>) driver.getWindowHandles();
+		Set<String> hashset3 = (HashSet<String>) driver.getWindowHandles();//to handle all the windows
 		List<String> list3 = new ArrayList<String>(hashset3);
 		System.out.println(list3.toString());
 
@@ -91,46 +77,51 @@ public class UploadContent
 		driver.switchTo().window(list3.get(1));
 		System.out.println(list3.get(1));
 
-		Thread.sleep(20000);				
+		Thread.sleep(5000);				
 
-		driver.findElement(By.xpath(properties.getProperty("vdropbox_signin"))).click();
+		driver.findElement(By.xpath(properties.getProperty("vdropbox_signin"))).click();//drop box sign in
 
 
-		WebElement emailidElement = driver.findElement(By.xpath(properties.getProperty("vdrop_box_username_click"))); 
-		emailidElement.sendKeys(properties.getProperty("vdrop_box_username"));
-		driver.findElement(By.xpath(properties.getProperty("vdrop_box_next_click"))).click();
+		WebElement emailidElement = driver.findElement(By.xpath(properties.getProperty("vdrop_box_username_click"))); //user name for drop box
+		emailidElement.sendKeys(properties.getProperty("vdrop_box_username"));//enter user name
+		driver.findElement(By.xpath(properties.getProperty("vdrop_box_next_click"))).click();//click next
 
 		Thread.sleep(3000);
 
-		WebElement EmailpwdElement = driver.findElement(By.name(properties.getProperty("vdrop_box_pwd_click"))); 
-		EmailpwdElement.sendKeys(properties.getProperty("vdrop_box_pwd"));
-		driver.findElement(By.xpath(properties.getProperty("vdrop_box_pwd_next"))).click();
+		WebElement EmailpwdElement = driver.findElement(By.name(properties.getProperty("vdrop_box_pwd_click"))); //password for drop box
+		EmailpwdElement.sendKeys(properties.getProperty("vdrop_box_pwd"));//enter password
+		driver.findElement(By.xpath(properties.getProperty("vdrop_box_pwd_next"))).click();//click for next
 
 		Thread.sleep(20000);
 		driver.findElement(By.xpath(properties.getProperty("cdropbox_item_select"))).click();
-		driver.findElement(By.xpath(properties.getProperty("cdrop_box_choose"))).click();
+		driver.findElement(By.xpath(properties.getProperty("cdrop_box_choose"))).click();//to click for selected item
 
-		Thread.sleep(8000);
-
-		driver.switchTo().window(list3.get(0));
-
-		driver.findElement(By.xpath(properties.getProperty("cdrop_box_after_home"))).click();
-		System.out.println("successfully uploaded the Content from DropBox");
 		Thread.sleep(5000);
 
+		driver.switchTo().window(list3.get(0));
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(properties.getProperty("cdrop_box_after_home"))).click();//click for home
+		System.out.println("successfully uploaded the Content from DropBox");
+		Thread.sleep(5000);
+		}
+		
+		@Test(priority=57,enabled=true)
+		public void upload_Box() throws InterruptedException {
+			
+		
 		//UploadClick-2//
 		Thread.sleep(3000);
-		WebElement ele2 = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));
+		WebElement ele2 = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));//hover to content
 		Actions act2 = new Actions(driver);
 		act2.moveToElement(ele2).perform();
 		Thread.sleep(5000);
-		driver.findElement(By.xpath(properties.getProperty("Upload_Click"))).click();
+		driver.findElement(By.xpath(properties.getProperty("Upload_Click"))).click();//click for upload content
 		Thread.sleep(10000);
 
 		//Box//
-		driver.findElement(By.xpath(properties.getProperty("cbox_click"))).click();
+		driver.findElement(By.xpath(properties.getProperty("cbox_click"))).click();//click for Box
 
-		Set<String> hashset4 = (HashSet<String>) driver.getWindowHandles();
+		Set<String> hashset4 = (HashSet<String>) driver.getWindowHandles();//to handle the windows
 		List<String> list4 = new ArrayList<String>(hashset4);
 		System.out.println(list4.toString());
 
@@ -141,33 +132,37 @@ public class UploadContent
 
 		Thread.sleep(12000);
 
-		WebElement EmailidElement1 = driver.findElement(By.name(properties.getProperty("vbox_login"))); 
-		EmailidElement1.sendKeys(properties.getProperty("vbox_Username"));
+		WebElement EmailidElement1 = driver.findElement(By.name(properties.getProperty("vbox_login"))); //login for box
+		EmailidElement1.sendKeys(properties.getProperty("vbox_Username"));//entered data for user name 
 
-		driver.findElement(By.xpath(properties.getProperty("vbox_login_submit"))).click();
+		driver.findElement(By.xpath(properties.getProperty("vbox_login_submit"))).click();//next
 
 		Thread.sleep(3000);
 
-		WebElement EmailpwdElement1 = driver.findElement(By.name(properties.getProperty("vbox_pwd_click"))); 
-		EmailpwdElement1.sendKeys(properties.getProperty("vbox_pwd"));
+		WebElement EmailpwdElement1 = driver.findElement(By.name(properties.getProperty("vbox_pwd_click"))); //click for password
+		EmailpwdElement1.sendKeys(properties.getProperty("vbox_pwd"));//enter data for password 
 
-		driver.findElement(By.xpath(properties.getProperty("vbox_submit"))).click();
+		driver.findElement(By.xpath(properties.getProperty("vbox_submit"))).click();//next
 
 		Thread.sleep(12000);
 
 		WebElement radio2=driver.findElement(By.xpath(properties.getProperty("cbox_item_radio")));
 		radio2.click();
 
-		driver.findElement(By.xpath(properties.getProperty("cbox_item_select"))).click();
+		driver.findElement(By.xpath(properties.getProperty("cbox_item_select"))).click();//selected item
 
 		Thread.sleep(5000);
 
 		driver.switchTo().window(list4.get(0));
 
-		driver.findElement(By.xpath(properties.getProperty("cbox_after_home"))).click();
+		driver.findElement(By.xpath(properties.getProperty("cbox_after_home"))).click();//click for home
 		System.out.println("successfully uploaded the Content from Box");
 		Thread.sleep(10000);
-
+		}
+		
+		@Test(priority=58,enabled=false)
+		public void upload_Googledrive() throws InterruptedException {
+			
 		//UploadClick-2//
 		Thread.sleep(3000);
 		WebElement ele3 = driver.findElement(By.xpath(properties.getProperty("Content_Mousehover")));
