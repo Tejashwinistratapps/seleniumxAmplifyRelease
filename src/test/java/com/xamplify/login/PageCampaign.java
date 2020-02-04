80268b7c21c781e0ace26fe6651c212903787409
lakshmig <lakshmig@LAP032.stratapps.com>
2020-02-04 14:29:06 +0530

    pc1

--- src/test/java/com/xamplify/login/PageCampaign.java
+++ src/test/java/com/xamplify/login/PageCampaign.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class PageCampaign {
 	
